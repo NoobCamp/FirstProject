d1a7fc9e98887790bd6afbd55532982eddbedf50
PunkN <79531121998@mail.ru>
2024-09-07 19:38:50 +0300

    fixed .gitignore and some files + tested commit system

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void Main(String[] args) {
-        System.out.println("shit happens");
+        System.out.println("shit happens!");
     }
 }
